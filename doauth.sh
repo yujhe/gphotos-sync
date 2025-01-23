8179d0df854b9b0134caafbc6b3ae6a0cb2a8c65
Jason <10588348+spraot@users.noreply.github.com>
2025-01-23 16:08:37 +0100

    Fix link in auth script

--- doauth.sh
+++ doauth.sh
@@ -15,6 +15,6 @@ echo "giving VNC time to be ready, please wait..."
 sleep 2
 
 echo "Open chrome by using the open-chrome.sh script then close that browser window (inside the container) before continuing"
-read -p  "Press any key after you have authenticated in your browser at http://$(hostname):6080/vnc_lite.html"
+read -p  "Press any key after you have authenticated in your browser at http://$(hostname):6080"
 
 docker compose down
