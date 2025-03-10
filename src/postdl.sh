25d2f35e0206525c0dc5eb0153d02691de4f715e
Jason <10588348+spraot@users.noreply.github.com>
2025-03-10 11:09:24 +0100

    Fix error if postdl made no changes

--- src/postdl.sh
+++ src/postdl.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-set -e
-
 if [ -z "$1" ];then
   echo "File argument required"
   exit 1
 fi
 
-exiftool "-datetimeoriginal<FileModifyDate" -P -overwrite_original_in_place -if 'not $DateTimeOriginal' "$1"
+exiftool "-datetimeoriginal<FileModifyDate" -P -overwrite_original_in_place -if 'not $DateTimeOriginal' "$1" > /dev/null 2>&1
+
+# If exit code is 2, it just means that this operation didn't make any change. Other exit codes should be propagated
+status=$?
+if [ $status -eq 2 ]; then
+  exit 0
+elif [ $status -ne 0 ]; then
+  exit $status
+fi
+
+echo "{\"level\": \"INFO\", \"message\": \"Added missing date in exif data for file: $(basename "$1")\", \"dt\": \"$(date '+%FT%T.%3N%:z')\"}"
