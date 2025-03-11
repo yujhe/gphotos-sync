156ad0063f7af344e6c36bb3e1a859f346808c3f
Jason <10588348+spraot@users.noreply.github.com>
2025-03-11 12:05:27 +0100

    Fix postdl log messages

--- src/postdl.sh
+++ src/postdl.sh
@@ -11,15 +11,15 @@ result=$(exiftool "-datetimeoriginal<FileModifyDate" -P -overwrite_original_in_p
 # If exit code is 2, it just means that this operation didn't make any change. Other exit codes should be propagated
 status=$?
 if [ $status -eq 2 ]; then
-  echo "{\"level\": \"debug\", \"message\": \"file already has date in exif data: $(basename \"$1\")\", \"dt\": \"$(date '+%FT%T.%3N%:z')\"}"
+  echo "{\"level\": \"debug\", \"message\": \"file already has date in exif data: $(basename "$1")\", \"dt\": \"$(date '+%FT%T.%3N%:z')\"}"
 elif [ $status -ne 0 ]; then
   # ignore error if result contains "too large" or "invalid atom size"
   if [[ "$result" == *"too large"* || "$result" == *"invalid atom size"* ]]; then
-    echo "{\"level\": \"info\", \"message\": \"skipping date update in exif data for $(basename \"$1\"), exiftool does not support this file\", \"dt\": \"$(date '+%FT%T.%3N%:z')\"}"
+    echo "{\"level\": \"info\", \"message\": \"skipping date update in exif data for $(basename "$1"), exiftool does not support this file\", \"dt\": \"$(date '+%FT%T.%3N%:z')\"}"
   fi
   while read line; do
     echo "{\"level\": \"error\", \"message\": \"exiftool: $line\", \"dt\": \"$(date '+%FT%T.%3N%:z')\"}"
   done <<< "$result"
 fi
 
-echo "{\"level\": \"info\", \"message\": \"Added missing date in exif data for $(basename \"$1\")\", \"dt\": \"$(date '+%FT%T.%3N%:z')\"}"
+echo "{\"level\": \"info\", \"message\": \"Added missing date in exif data for $(basename "$1")\", \"dt\": \"$(date '+%FT%T.%3N%:z')\"}"
