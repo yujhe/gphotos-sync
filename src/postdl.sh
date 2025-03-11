baf53fa8bba67582395c42570442f1262345b9a6
Jason <10588348+spraot@users.noreply.github.com>
2025-03-11 10:49:53 +0100

    print exiftool error message

--- src/postdl.sh
+++ src/postdl.sh
@@ -5,13 +5,15 @@ if [ -z "$1" ];then
   exit 1
 fi
 
-exiftool "-datetimeoriginal<FileModifyDate" -P -overwrite_original_in_place -if 'not $DateTimeOriginal' "$1" > /dev/null 2>&1
+result=$(exiftool "-datetimeoriginal<FileModifyDate" -P -overwrite_original_in_place -if 'not $DateTimeOriginal' "$1" 2>&1)
 
 # If exit code is 2, it just means that this operation didn't make any change. Other exit codes should be propagated
 status=$?
 if [ $status -eq 2 ]; then
   exit 0
 elif [ $status -ne 0 ]; then
+  echo "exiftool failed:"
+  echo "$result"
   exit $status
 fi
 
